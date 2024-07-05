23bd797f0b0a437e3d5940ee2bfd859fb3d7b0c4
Ryann0va <rhehn@usf.edu>
2024-07-05 17:34:10 -0400

    Create PersonalInfo.js

---
+++ PersonalInfo.js
@@ -0,0 +1,15 @@
+//U99350821
+import React from 'react';
+import './PersonalInfo.css';
+
+const PersonalInfo = () => {
+  return (
+    <div className="personal-info">
+      <h1>Ryan Hehn</h1>
+      <p>An aspiring programmer hoping to make way into the buisness world.</p>
+      <p>Email: rhehn@usf.edu</p>
+    </div>
+  );
+};
+
+export default PersonalInfo;
